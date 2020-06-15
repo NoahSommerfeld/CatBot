ae2010b084f3ecab57f720e0a61f6433b257c323
maestro_master <noah.sommerfeld@gmail.com>
2020-06-14 17:58:37 -0700

    now non-blocking while waiting for packet components, and configured with a timeout.

--- Arduino/Src/Main/Postman.ino
+++ Arduino/Src/Main/Postman.ino
@@ -1,61 +1,57 @@
-/* Takes in a packet via serial connection and echoes it back.
+/* TCP-level functionality to handle serial connections and abstract all non-payload logic from MAIN
 - uses a made-up protocol:
 - packet start symbol - '|'
 - packet end symbol - '+'
 - packet structure: TBD
 */
 
+#define PACKETSTARTSYMBOL '|'
+#define PACKETENDSYMBOL '+'
+#define TIMEOUT 2000 //timeout value in milliseconds
 
-
-
-
+long lastUpdateTime = 99999999;
+boolean isRecording = false;
+String pendingPacket = "";
 
 //parses any pending commands 
 String refreshMailbox(){
-  //serial.println("starting read");
-  String tempCmd = "";
-  int parseCount=0;
   char tempChar = '\0';
-  if (Serial.available() > 0) {
+  while(Serial.available() > 0) {
     int tempData = Serial.read();
     tempChar = tempData;  
-  //  Serial.println("got a character: '"+String(tempChar)+"' Ascii:"+tempData);
-  }
-  else{
-    return "";
-  }
-
-  if(tempChar =='|'){ //start of packet
- //   Serial.println("got a startOfPacket");
-    while(parseCount<10000){ //to avoid an infinite loop. TODO - update to use millis vs arb. clock cycles
-      parseCount+=1;
-      char tempChar = '\0'; 
-      if (Serial.available() > 0) {
-        int tempData2 = Serial.read();
-        tempChar = tempData2;
-       }
-       else{
-        continue;
-       }
-
-      if(tempChar=='+'){ //end of packet
-        //Serial.println("got an endofpacket");
-        return tempCmd;
-      }
-      else if(tempChar == '|' || tempChar == 10){
-      //  Serial.println("discarding char: '"+String(tempChar)+"'");
-        //repeating start symbol or non text, do nothing
-      }
-      else{
-      //  Serial.println("adding char '"+String(tempChar)+"'");
-        if(tempChar != '\0'){
-          tempCmd += tempChar;
-        }
-      }      
+     Serial.println("got a character: '"+String(tempChar)+"' Ascii:"+tempData);
+  
+    if(tempChar ==PACKETSTARTSYMBOL){ //start of packet
+      //   Serial.println("got a startOfPacket");
+      isRecording = true;
+      lastUpdateTime = millis();
+      continue;
     }
+    if(tempChar==PACKETENDSYMBOL){ //end of packet
+      //Serial.println("got an endofpacket");
+      String tempCmd = pendingPacket;
+      isRecording = false; 
+      pendingPacket = "";
+      return tempCmd;
+    }
+    else if(tempChar == PACKETSTARTSYMBOL || tempChar == 10){
+      //  Serial.println("discarding char: '"+String(tempChar)+"'");
+      //repeating start symbol or non text, do nothing
+    }
+    else if(isRecording){
+      //  Serial.println("adding char '"+String(tempChar)+"'");
+      if(tempChar != '\0'){
+        pendingPacket += tempChar;
+        lastUpdateTime = millis();
+      }
+    }      
+  }
+  if(isRecording && pendingPacket.length() != 0 && (millis() - lastUpdateTime)>TIMEOUT){
+      pendingPacket="";
+      isRecording = false;
+      return "TIMEOUT";    //timeout - drop the data that had been received
   }
   else{
     return "";
   }
-  return "timeout"; //timeout - drop the data that had been received
 }
