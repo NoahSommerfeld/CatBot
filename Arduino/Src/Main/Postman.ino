09464b98faaddfc7b7107d5641926de1815841c2
maestro_master <noah.sommerfeld@gmail.com>
2020-06-14 17:09:16 -0700

    Commented out logging to keep memory footprint low

--- Arduino/Src/Main/Postman.ino
+++ Arduino/Src/Main/Postman.ino
@@ -19,35 +19,35 @@ String refreshMailbox(){
   if (Serial.available() > 0) {
     int tempData = Serial.read();
     tempChar = tempData;  
-    Serial.println("got a character: '"+String(tempChar)+"' Ascii:"+tempData);
+  //  Serial.println("got a character: '"+String(tempChar)+"' Ascii:"+tempData);
   }
   else{
     return "";
   }
 
   if(tempChar =='|'){ //start of packet
-    Serial.println("got a startOfPacket");
+ //   Serial.println("got a startOfPacket");
     while(parseCount<10000){ //to avoid an infinite loop. TODO - update to use millis vs arb. clock cycles
       parseCount+=1;
       char tempChar = '\0'; 
-       if (Serial.available() > 0) {
-      int tempData2 = Serial.read();
-      tempChar = tempData2;
+      if (Serial.available() > 0) {
+        int tempData2 = Serial.read();
+        tempChar = tempData2;
        }
        else{
         continue;
        }
 
       if(tempChar=='+'){ //end of packet
-        Serial.println("got an endofpacket");
+        //Serial.println("got an endofpacket");
         return tempCmd;
       }
       else if(tempChar == '|' || tempChar == 10){
-        Serial.println("discarding char: '"+String(tempChar)+"'");
+      //  Serial.println("discarding char: '"+String(tempChar)+"'");
         //repeating start symbol or non text, do nothing
       }
       else{
-        Serial.println("adding char '"+String(tempChar)+"'");
+      //  Serial.println("adding char '"+String(tempChar)+"'");
         if(tempChar != '\0'){
           tempCmd += tempChar;
         }
